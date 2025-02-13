511fafd68869d03947e6a0be57ed4afe76b401b3
Melisa Lachev <melisatormac55@gmail.com>
2025-02-13 18:44:13 +0200

    make a draft for operator and number buttons

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-let operator;
-let num1;
-let num2;
+let operators = document.querySelectorAll('.operator-btn')
+let numbers = document.querySelectorAll('.number-btn')
+let display = document.getElementById('display')
+let clear = document.querySelector(".clear");
+let equal = document.querySelector(".equal");
+
+let num1 = [];
+let num2 = [];
+let operator = [];
+
+ for (let i = 0; i < operators.length; i++) {
+    operators[i].addEventListener('click' , () => {
+        display.textContent += operators[i].textContent
+      })
+    }
+
+for (let i = 0; i < numbers.length; i++) {
+        numbers[i].addEventListener('click' , () => {
+            display.textContent += numbers[i].textContent
+          })
+        } 
+
+clear.addEventListener("click", () => {
+    display.textContent = ``
+})
 
 function addNumbers(num1,num2) {
     return num1 + num2
@@ -13,5 +35,18 @@ function multiplyNumbers(num1,num2) {
 }
 function divideNumbers(num1,num2) {
     return num1 / num2
+}
+
+function operateCalculator(num1,num2, operator){
+    switch (operator) {
+        case '+':
+            return addNumbers(num1,num2)
+        case '-':
+            return substractNumbers(num1,num2)
+        case '*':
+            return multiplyNumbers(num1,num2)
+        case '/':
+            return divideNumbers(num1,num2)
+    }
 }
 
