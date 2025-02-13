3a4163cafdef26dae99dbbde8fe9c16b424a3097
Melisa Lachev <melisatormac55@gmail.com>
2025-02-13 19:56:20 +0200

    make calculator functional

--- script.js
+++ script.js
@@ -1,52 +1,67 @@
 let operators = document.querySelectorAll('.operator-btn')
-let numbers = document.querySelectorAll('.number-btn')
+let numbers = document.querySelectorAll('.number')
 let display = document.getElementById('display')
 let clear = document.querySelector(".clear");
 let equal = document.querySelector(".equal");
 
-let num1 = [];
-let num2 = [];
-let operator = [];
+let num1 = ``;
+let num2 = ``;
+let operator = ``;
+let sum = ``;
 
- for (let i = 0; i < operators.length; i++) {
+
+function operateCalculator(num1, num2, operator){
+    switch (operator) {
+        case '+':
+            return num1 + num2 
+        case '-':
+            return num1 - num2
+        case '*':
+            return num1 * num2
+        case '/':
+            return num1 / num2
+    }
+    console.log(num1, num2, operator)
+}
+
+
+
+
+for (let i = 0; i < operators.length; i++) {
     operators[i].addEventListener('click' , () => {
         display.textContent += operators[i].textContent
+        operator = operators[i].textContent
+        console.log(operator)
+
       })
     }
 
 for (let i = 0; i < numbers.length; i++) {
         numbers[i].addEventListener('click' , () => {
             display.textContent += numbers[i].textContent
+            if (operator == ``) {
+                num1 += numbers[i].textContent;
+                console.log(num1)
+
+            } else {
+                num2 += numbers[i].textContent;
+                console.log(num2)
+
+            }
+
           })
         } 
 
+ equal.addEventListener('click', ()=> {
+     sum = operateCalculator(Number(num1),Number(num2), operator)
+     display.textContent = sum
+
+
+ })
+
 clear.addEventListener("click", () => {
     display.textContent = ``
+    num1 = ``
+    num2 = ``
+    operator = ``
 })
-
-function addNumbers(num1,num2) {
-    return num1 + num2
-}
-function substractNumbers(num1,num2) {
-    return num1 - num2
-}
-function multiplyNumbers(num1,num2) {
-    return num1 * num2
-}
-function divideNumbers(num1,num2) {
-    return num1 / num2
-}
-
-function operateCalculator(num1,num2, operator){
-    switch (operator) {
-        case '+':
-            return addNumbers(num1,num2)
-        case '-':
-            return substractNumbers(num1,num2)
-        case '*':
-            return multiplyNumbers(num1,num2)
-        case '/':
-            return divideNumbers(num1,num2)
-    }
-}
-
